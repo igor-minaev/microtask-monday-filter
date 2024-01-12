3cedb972e7655ecf3e95743b90f7d049459e41c8
Igor Minayeu <igor.minayeu@gmail.com>
2024-01-12 21:19:17 +0300

    start microtask filter

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+    const [money, setMoney] = useState([
+        {banknote: "dollar", nominal: 100, number: "a123456789"},
+        {banknote: "dollar", nominal: 50, number: "b123456789"},
+        {banknote: "ruble", nominal: 100, number: "c123456789"},
+        {banknote: "dollar", nominal: 100, number: "d123456789"},
+        {banknote: "dollar", nominal: 50, number: "e123456789"},
+        {banknote: "ruble", nominal: 100, number: "f123456789"},
+        {banknote: "dollar", nominal: 50, number: "j123456789"},
+        {banknote: "ruble", nominal: 50, number: "h123456789"}
+    ])
+    return (
+        <ul>
+            {money.map((objFromMoneyArr) => {
+                return (
+                    <li key={objFromMoneyArr.number}>
+                        <span>{objFromMoneyArr.banknote} </span>
+                        <span>{objFromMoneyArr.nominal} </span>
+                        <span>{objFromMoneyArr.number}</span>
+                    </li>
+                )
+            })}
+        </ul>
+    );
+}
+
+export default App;
